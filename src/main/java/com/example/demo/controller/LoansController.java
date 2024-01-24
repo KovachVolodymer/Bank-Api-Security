5f58ddb6d95efbab2f59c82c9912aa814c6c5604
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-01-24 12:51:57 +0200

    created config

--- src/main/java/com/example/demo/controller/LoansController.java
+++ src/main/java/com/example/demo/controller/LoansController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoansController {
 
+    @PreAuthorize("hasAnyRole('ROLE_USER')")
     @GetMapping("/myLoans")
     public String getLoans() {
         return "Hello from LoansController";
--- src/main/java/com/example/demo/controller/NoticesController.java
+++ src/main/java/com/example/demo/controller/NoticesController.java
@@ -1,2 +1,15 @@
-package com.example.demo.controller;public class NoticesController {
+package com.example.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+    
+    @GetMapping("/notices")
+    public String getNotices() {
+        return "Hello from NoticesController";
+    }
 }
