dd921ef3efce3e2bd278da042dd971e6ae7d1833
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-02-06 16:57:00 +0200

    small fixed a bug

--- src/main/java/com/example/demo/repository/NoticeRepository.java
+++ src/main/java/com/example/demo/repository/NoticeRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.Query;
@@ -14,14 +14,4 @@ public interface NoticeRepository extends CrudRepository<Notice, Long> {
 
     @Query(value = "{ 'noticeEndDt' : { $gte : ?0 } }")
     List<Notice> findAllActiveNotices();
-
-=======
-import com.example.demo.model.Notice;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface NoticeRepository extends MongoRepository<Notice, String>{
-    List<Notice> findAllActiveNotices();
->>>>>>> c5af303323aff78faaab538d5168eb89b698b171
 }
