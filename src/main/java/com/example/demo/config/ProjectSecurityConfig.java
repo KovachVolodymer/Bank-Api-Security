a3ca01ada706fac924f0aeb05372e3671c50fd17
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-02-07 16:27:52 +0200

    setting cors

--- src/main/java/com/example/demo/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/demo/config/ProjectSecurityConfig.java
@@ -1,21 +1,40 @@
 package com.example.demo.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
 public class ProjectSecurityConfig {
-
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("https://example.com"));
+                        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST"));
+                        return corsConfiguration;
+                    }
+                })).csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/notices","/contact","/register").permitAll()
                         .anyRequest().authenticated()
@@ -29,5 +48,7 @@ public class ProjectSecurityConfig {
         int strength = 10; // work factor of bcrypt
         return new BCryptPasswordEncoder(strength);
     }
+
+
 
 }
