4aa659f65bdcfbea9942de5f2231fdba9c92cd8d
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-02-09 23:50:02 +0200

    update balance

--- src/main/java/com/example/demo/controller/BalanceController.java
+++ src/main/java/com/example/demo/controller/BalanceController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.AccountTransactions;
 import com.example.demo.repository.AccountTransactionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,39 @@ public class BalanceController {
             return null;
         }
     }
+
+    // Add a new endpoint to get the balance details for a specific account
+    @GetMapping("/accountBalance")
+    public List<AccountTransactions> getAccountBalanceDetails(@RequestParam String id) {
+        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                findByAccountIdOrderByTransactionDtDesc(id);
+        if (accountTransactions != null ) {
+            return accountTransactions;
+        }else {
+            return null;
+        }
+    }
+
+    @PostMapping("/addBalance")
+    public String addBalance(@RequestParam String id, @RequestParam String amount) {
+        AccountTransactions accountTransactions = new AccountTransactions();
+        accountTransactions.setAccountId(id);
+        accountTransactions.setTransactionAmount(Double.parseDouble(amount));
+        accountTransactions.setTransactionType("Credit");
+        accountTransactionsRepository.save(accountTransactions);
+        return "Amount added successfully";
+    }
+
+    @PostMapping("/withdrawBalance")
+    public String withdrawBalance(@RequestParam String id, @RequestParam String amount) {
+        AccountTransactions accountTransactions = new AccountTransactions();
+        accountTransactions.setAccountId(id);
+        accountTransactions.setTransactionAmount(Double.parseDouble(amount));
+        accountTransactions.setTransactionType("Debit");
+        accountTransactionsRepository.save(accountTransactions);
+        return "Amount withdrawn successfully";
+    }
+
+
+
 }
