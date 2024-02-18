536c0330b0ff382c0eafb408a572cecdcff4ca58
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-02-18 21:31:21 +0200

    update cardsController

--- src/main/java/com/example/demo/controller/CardsController.java
+++ src/main/java/com/example/demo/controller/CardsController.java
@@ -3,11 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.model.Cards;
 import com.example.demo.repository.CardsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -25,7 +24,35 @@ public class CardsController {
         }
     }
 
+    @GetMapping("/all")
+    public List<Object> findAll(){
+        List<Cards> cards= cardsRepository.findAll();
+        return Collections.singletonList(cards);
+    }
 
+    @PostMapping("/add")
+    public ResponseEntity<Object> addCard(@RequestBody Cards card){
+        cardsRepository.save(card);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Object> deleteCard(@RequestParam String id){
+        cardsRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateCard(@RequestBody Cards card){
+        cardsRepository.save(card);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/patch")
+    public ResponseEntity<Object> patchCard(@RequestBody Cards card){
+        cardsRepository.save(card);
+        return ResponseEntity.ok().build();
+    }
 
 
 
